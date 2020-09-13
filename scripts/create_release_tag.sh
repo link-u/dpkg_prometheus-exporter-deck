2c0d806091014a2562fdb55b9dda483364ba530a
psi <ryo.hirafuji@link-u.co.jp>
2020-09-13 15:58:35 +0900

    Fix tag

--- scripts/create_release_tag.sh
+++ scripts/create_release_tag.sh
@@ -17,6 +17,6 @@ SCRIPT_PATH=$(cd $(dirname $(__readlink_f $0)) && pwd)
 cd ${SCRIPT_PATH}
 cd ..
 
-tag=$(git -C gpac describe --tags | sed 's/\(.*\)-.*/\1/')
+tag=$(git describe --tags || (echo -n "v0.0.0-" && git describe --always))
 git tag ${tag}
 git push origin ${tag}

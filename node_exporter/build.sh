baa730abb235d1d6fca068032156762b1f973aa3
psi <ryo.hirafuji@link-u.co.jp>
2020-09-13 15:49:31 +0900

    override build and clean

---
+++ node_exporter/build.sh
@@ -0,0 +1,8 @@
+#! /bin/bash -eux
+
+set -eux
+
+SCRIPT_PATH=$(cd $(dirname $(readlink -f $0)) && pwd)
+cd ${SCRIPT_PATH}
+
+env --chdir=${SCRIPT_PATH}/node_exporter make
--- node_exporter/configure.sh
+++ node_exporter/configure.sh
@@ -4,8 +4,5 @@ set -eux
 
 SCRIPT_PATH=$(cd $(dirname $(readlink -f $0)) && pwd)
 cd ${SCRIPT_PATH}
-cd ..
 
-fakeroot debian/rules clean
-fakeroot debian/rules configure
-fakeroot debian/rules binary
+env --chdir=${SCRIPT_PATH}/node_exporter go mod download

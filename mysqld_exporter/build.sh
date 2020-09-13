9b9b7b73aee90e923a0f41bd2b9d8ceffd7ffdcd
psi <ryo.hirafuji@link-u.co.jp>
2020-09-14 06:27:29 +0900

    install mysqld_exporter

---
+++ mysqld_exporter/build.sh
@@ -0,0 +1,9 @@
+#! /bin/bash -eux
+
+set -eux
+
+SCRIPT_PATH=$(cd $(dirname $(readlink -f $0)) && pwd)
+cd ${SCRIPT_PATH}
+
+env --chdir=${SCRIPT_PATH}/mysqld_exporter go mod download
+env --chdir=${SCRIPT_PATH}/mysqld_exporter make
---
+++ mysqld_exporter/install.sh
@@ -0,0 +1,8 @@
+#! /bin/bash -eux
+
+set -eux
+
+SCRIPT_PATH=$(cd $(dirname $(readlink -f $0)) && pwd)
+cd ${SCRIPT_PATH}
+
+cp ./mysqld_exporter/mysqld_exporter /usr/bin

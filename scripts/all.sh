8d98ea4fac546380c70ff5c1783c368b0a87333f
psi <ryo.hirafuji@link-u.co.jp>
2020-09-13 15:28:05 +0900

    add description

--- scripts/all.sh
+++ scripts/all.sh
@@ -13,8 +13,8 @@ mk-build-deps --install --remove \
   --tool='apt-get -o Debug::pkgProblemResolver=yes --no-install-recommends --yes' \
   debian/control
 
-# Install golang manually
-
+# Install the latest golang manually
+# https://github.com/canha/golang-tools-install-script
 curl -L https://raw.githubusercontent.com/canha/golang-tools-install-script/master/goinstall.sh | bash
 
 bash scripts/build.sh

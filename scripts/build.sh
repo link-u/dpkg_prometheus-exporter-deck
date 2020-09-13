01b4a400f9216228640a02f2704a791055e28424
psi <ryo.hirafuji@link-u.co.jp>
2020-09-13 16:20:07 +0900

    eval go env

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,6 +6,8 @@ SCRIPT_PATH=$(cd $(dirname $(readlink -f $0)) && pwd)
 cd ${SCRIPT_PATH}
 cd ..
 
+eval "$(go env)"
+
 fakeroot debian/rules clean
 fakeroot debian/rules configure
 fakeroot debian/rules binary

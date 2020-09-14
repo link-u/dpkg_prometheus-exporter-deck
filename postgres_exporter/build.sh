16284a2277117865c70b7e5e038a5d33476a2430
psi <ryo.hirafuji@link-u.co.jp>
2020-09-14 11:27:11 +0900

    Fix build process. Use GOPATH

--- postgres_exporter/build.sh
+++ postgres_exporter/build.sh
@@ -6,5 +6,9 @@ SCRIPT_PATH=$(cd $(dirname $(readlink -f $0)) && pwd)
 cd ${SCRIPT_PATH}
 cd postgres_exporter
 
-go get -u -d github.com/wrouesnel/postgres_exporter
+pushd
+cd "${GOPATH}/src/github.com/wrouesnel/postgres_exporter"
 go run mage.go binary
+popd
+
+cp -vf "${GOPATH}/src/github.com/wrouesnel/postgres_exporter/postgres_exporter" .

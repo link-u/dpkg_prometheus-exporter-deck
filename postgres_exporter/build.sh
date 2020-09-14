2bba3efad528b34d89abdb2afd068527cbb8eca8
psi <ryo.hirafuji@link-u.co.jp>
2020-09-14 11:38:03 +0900

    enable cgo?

--- postgres_exporter/build.sh
+++ postgres_exporter/build.sh
@@ -6,13 +6,8 @@ SCRIPT_PATH=$(cd $(dirname $(readlink -f $0)) && pwd)
 cd ${SCRIPT_PATH}
 cd postgres_exporter
 
-go get -u -d go get github.com/wrouesnel/postgres_exporter
-rm -Rf "${GOPATH}/src/github.com/wrouesnel/postgres_exporter"
-cp postgres_exporter "${GOPATH}/src/github.com/wrouesnel/postgres_exporter"
-pushd
-cd "${GOPATH}/src/github.com/wrouesnel/postgres_exporter"
-go get .
+export CGO_ENABLED=1
+go mod vendor
 go run mage.go binary
-popd
 
 cp -vf "${GOPATH}/src/github.com/wrouesnel/postgres_exporter/postgres_exporter" .

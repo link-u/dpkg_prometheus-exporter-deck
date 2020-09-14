966ca37a92c011aa817a1ee64ec9087ddf14a21a
psi <ryo.hirafuji@link-u.co.jp>
2020-09-14 11:25:31 +0900

    Fix build process

--- postgres_exporter/build.sh
+++ postgres_exporter/build.sh
@@ -6,5 +6,5 @@ SCRIPT_PATH=$(cd $(dirname $(readlink -f $0)) && pwd)
 cd ${SCRIPT_PATH}
 cd postgres_exporter
 
-go get -u -d github.com/magefile/mage
+go get -u -d github.com/wrouesnel/postgres_exporter
 go run mage.go binary

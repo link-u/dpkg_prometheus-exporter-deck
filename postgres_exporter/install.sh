6f6bb877688ca62d96da636c8be082ad535e3cb5
psi <ryo.hirafuji@link-u.co.jp>
2020-09-14 10:52:14 +0900

    add postgres_exporter

---
+++ postgres_exporter/install.sh
@@ -0,0 +1,8 @@
+#! /bin/bash -eux
+
+set -eux
+
+SCRIPT_PATH=$(cd $(dirname $(readlink -f $0)) && pwd)
+cd ${SCRIPT_PATH}
+
+install -D -m 755 ./postgres_exporter/postgres_exporter ../debian/tmp/usr/bin

95ed8b0ac5b59830f85cd3a355e7b80f0ca4d784
psi <ryo.hirafuji@link-u.co.jp>
2020-10-29 23:36:51 +0900

    Fix build process

--- postgres_exporter/build.sh
+++ postgres_exporter/build.sh
@@ -7,4 +7,9 @@ cd ${SCRIPT_PATH}
 cd postgres_exporter
 
 apt-get install -y postgresql-client-common
+
+export CGO_ENABLED=1
+export GO111MODULE=off
+go get github.com/magefile/mage
+go run mage.go
 go build -o postgres_exporter ./cmd/postgres_exporter

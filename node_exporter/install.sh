ae1a915144ec7a90c6bccb2fcc3db949a8fbcba7
psi <ryo.hirafuji@link-u.co.jp>
2020-10-30 00:33:03 +0900

    rename dir

--- node_exporter/install.sh
+++ node_exporter/install.sh
@@ -5,4 +5,4 @@ set -eux
 SCRIPT_PATH=$(cd $(dirname $(readlink -f $0)) && pwd)
 cd ${SCRIPT_PATH}
 
-install -D -m 755 ./node_exporter/node_exporter ../debian/tmp/usr/bin
+install -D -m 755 ./node_exporter/node_exporter ../prometheus-exporter-deck/debian/tmp/usr/bin
--- sql_exporter/install.sh
+++ sql_exporter/install.sh
@@ -5,4 +5,4 @@ set -eux
 SCRIPT_PATH=$(cd $(dirname $(readlink -f $0)) && pwd)
 cd ${SCRIPT_PATH}
 
-install -D -m 755 ./sql_exporter/sql_exporter ../debian/tmp/usr/bin
+install -D -m 755 ./sql_exporter/sql_exporter ../prometheus-exporter-deck/debian/tmp/usr/bin

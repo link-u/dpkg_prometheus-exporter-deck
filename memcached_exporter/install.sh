7d6aaff517e43dee0c18dc5cbca798b7a29d5503
psi <ryo.hirafuji@link-u.co.jp>
2020-11-02 15:39:09 +0900

    install into sbin

--- memcached_exporter/install.sh
+++ memcached_exporter/install.sh
@@ -5,4 +5,4 @@ set -eux
 SCRIPT_PATH=$(cd $(dirname $(readlink -f $0)) && pwd)
 cd ${SCRIPT_PATH}
 
-install -D -m 755 ./memcached_exporter/memcached_exporter ../prometheus-exporter-deck/debian/tmp/usr/bin
+install -D -m 755 ./memcached_exporter/memcached_exporter ../prometheus-exporter-deck/debian/tmp/usr/sbin
